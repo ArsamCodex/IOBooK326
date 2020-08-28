f26c1ca6e63da7a6107209ffd82448811d28cb46
ARII <62879887+ArsamCodex@users.noreply.github.com>
2020-08-28 17:12:38 +0200

    I

---
+++ src/IO/Example10_14.java
@@ -0,0 +1,17 @@
+package IO;
+
+public class Example10_14 {
+    public static void main(String [ ] args)
+    {
+
+    }
+
+    public static void writeData(String outputFile){
+
+    }
+    public static void editData(String inputFile){
+
+    }
+    public static void printData(String File){}
+    
+}
