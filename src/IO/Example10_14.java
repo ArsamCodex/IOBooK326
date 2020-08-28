59ceb42b9c69c62774a0aa8dbd3845d6c9f4e7b3
ARII <62879887+ArsamCodex@users.noreply.github.com>
2020-08-28 17:33:09 +0200

    II

--- src/IO/Example10_14.java
+++ src/IO/Example10_14.java
@@ -6,7 +6,24 @@ public class Example10_14 {
 
     }
 
-    public static void writeData(String outputFile){
+    public static void writeData(String outputFile) throws FileNotFoundException {
+        PrintWriter outFile = new PrintWriter(outputFile);
+        String name;
+        int stNo;
+        double grade;
+        System.out.println("ENTER STUDENT NAME\n");
+        name = console.next();
+        while(!name.trim().equals("stop")){
+            System.out.println("\nENTER STUDENT NUMBER AND GRADE\n");
+            stNo = console.nextInt();
+            grade = console.nextDouble();
+            outFile.write(name+ "");
+            outFile.write(stNo);
+            outFile.write(grade + "");
+            System.out.println("\nENTER A NAME\n");
+            name = console.next();
+        }
+        outFile.close();
 
     }
     public static void editData(String inputFile){
