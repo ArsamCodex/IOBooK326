568050a44a9faa9057f8ae7c53f201471cce9cf6
ARII <62879887+ArsamCodex@users.noreply.github.com>
2020-08-29 13:20:37 +0200

    III

--- src/IO/Example10_14.java
+++ src/IO/Example10_14.java
@@ -1,36 +1,62 @@
 package IO;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Example10_14 {
-    public static void main(String [ ] args)
-    {
-         String fileName = "seqfile.dat";
-        // writeData(fileName);
+    static Scanner console = new Scanner(System.in);
+    public static void main(String [ ] args) throws FileNotFoundException {
+        String fileName = "seqfile.dat";
+       writeData(fileName);
+       // printData(fileName);
 
     }
+    
+    //WRITE DATA METHOD WRITES INPUT NOT LINE BY LINE BUT APPENDED . A BUG 
 
     public static void writeData(String outputFile) throws FileNotFoundException {
         PrintWriter outFile = new PrintWriter(outputFile);
         String name;
         int stNo;
-        double grade;
-        System.out.println("ENTER STUDENT NAME\n");
+        int grade;
+        System.out.print("ENTER STUDENT NAME\n");
         name = console.next();
         while(!name.trim().equals("stop")){
-            System.out.println("\nENTER STUDENT NUMBER AND GRADE\n");
+            System.out.print("\nENTER STUDENT NUMBER \n");
             stNo = console.nextInt();
-            grade = console.nextDouble();
+            System.out.println("ENTER GRADE NU");
+            grade = console.nextInt();
             outFile.write(name+ "");
             outFile.write(stNo);
             outFile.write(grade + "");
-            System.out.println("\nENTER A NAME\n");
+            System.out.print("\nENTER A NAME\n");
             name = console.next();
         }
         outFile.close();
 
     }
     public static void editData(String inputFile){
+        
 
     }
-    public static void printData(String File){}
-    
+    public static void printData(String file) throws FileNotFoundException {
+        String name ;
+        int stNo;
+        int grade;
+        Scanner inFile = new Scanner (new FileReader(file));
+        while(true){
+            if(inFile.hasNext()){
+                name = inFile.next();
+                stNo = inFile.nextInt();
+                grade = inFile.nextInt();
+                System.out.println(name + "   " + stNo + "   " + grade);
+            }
+            else
+                break;
+        }
+        System.out.println("END FILE CLOSING ...");
+        inFile.close();;    }
+
 }
